4cae3bc0be932a917ed55fbbd050674d4e7a872e
dickensodera <dickens@laevitas.ch>
2024-06-24 13:45:12 +0300

    update: check data balance

--- contracts/PaymentEscrow.sol
+++ contracts/PaymentEscrow.sol
@@ -1,16 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.0;
+import { AccessControl } from '@openzeppelin/contracts/access/AccessControl.sol';
+import { ReentrancyGuard } from '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
 
-contract PaymentEscrow{
-    public function releaseCUSD() external{
+contract PaymentEscrow is AccessControl, ReentrancyGuard{
+    bytes32 public constant FUND_MANAGER_ROLE = keccak256('FUND_MANAGER_ROLE');
+
+    constructor(){
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(FUND_MANAGER_ROLE, msg.sender);
+    }
+
+    function releaseCUSD() onlyRole(FUND_MANAGER_ROLE) nonReentrant external{
 
     }
 
-    public function deposit() external{
+    function deposit() nonReentrant external{
 
     }
 
-    public function getEscrowBalance() external{
-        
+    function getEscrowBalance() nonReentrant external{
+
     }
 }
