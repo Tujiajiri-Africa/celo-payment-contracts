9437b266e50833c57c03ec8b4b95731809c4fb61
dickensodera <dickens@laevitas.ch>
2024-06-24 13:34:02 +0300

    update: Initial setup of contract functions

---
+++ contracts/PaymentEscrow.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+contract PaymentEscrow{
+    public function releaseCUSD() external{
+
+    }
+
+    public function deposit() external{
+
+    }
+
+    public function getEscrowBalance() external{
+        
+    }
+}
