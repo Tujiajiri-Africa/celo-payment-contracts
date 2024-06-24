6c9e3ee545c72114bb38da1eb433f09ee7b95356
dickensodera <dickens@laevitas.ch>
2024-06-24 13:54:38 +0300

    update: enable contract owner to send ether

--- contracts/PaymentEscrow.sol
+++ contracts/PaymentEscrow.sol
@@ -6,13 +6,34 @@ import { ReentrancyGuard } from '@openzeppelin/contracts/utils/ReentrancyGuard.s
 contract PaymentEscrow is AccessControl, ReentrancyGuard{
     bytes32 public constant FUND_MANAGER_ROLE = keccak256('FUND_MANAGER_ROLE');
 
+    error INVALID_RECIPIENT;
+    error INVALID_ASSET_AMOUNT;
+
+    event Pay(
+        address indexed recipient,
+        uint256 indexed amount,
+        uint256 indexed timestamp
+
+    );
+
+    event Deposit(
+        address indexed sender,
+        uint256 indexed amount,
+        uint256 indexed timestamp
+
+    );
+
     constructor(){
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(FUND_MANAGER_ROLE, msg.sender);
     }
 
-    function releaseCUSD() onlyRole(FUND_MANAGER_ROLE) nonReentrant external{
+    function releaseCUSD(address payable _recipient, uint256 _amount) onlyRole(FUND_MANAGER_ROLE) nonReentrant external{
+        if(_recipient == address(0)) revert INVALID_RECIPIENT();
+        if(_amount == 0) revert INVALID_ASSET_AMOUNT();
 
+        (bool sent, bytes memory data) = _recipient.call{value: _amount}(""); //msg.value
+        require(sent, "Failed to send Asset");
     }
 
     function deposit() nonReentrant external{
